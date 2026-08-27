fb2bde0147e328212ab4f0eee90684c236b0f3e0
Suzin <7042047+suzinyou@users.noreply.github.com>
2026-08-27 14:12:28 +0530

    fix module rename

--- src/decodability/kiswahili/scorers.py
+++ src/decodability/kiswahili/scorers.py
@@ -3,7 +3,7 @@ from functools import reduce
 from decodability.kiswahili.segment import (
     get_clusters,
 )
-from decodability.kiswahili.models import KiswahiliStudentKnowledge
+from decodability.kiswahili.student_knowledge import KiswahiliStudentKnowledge
 from decodability.kiswahili.segment import get_grapheme_symbols
 
 
